1eeddf0610bd6917a2ee51b0b6351a3642a87ea5
YassineAtigui <atiguiyassine03@gmail.com>
2024-12-29 19:59:05 +0100

    First commit (Testing api)

---
+++ config_template.py
@@ -0,0 +1,8 @@
+API_KEYS = {
+    "alpaca": {
+        "api_key": "PKAYAGEKG1GMHEO5I3AN",
+        "secret_key": "oZh9vVMxCwIoOCXBJrqVYakhmSK7uS8CMuur9o4a",
+        "ENDPOINT":"https://paper-api.alpaca.markets/v2",
+        "paper_trading": True
+    }
+}
