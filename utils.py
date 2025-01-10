153529457b7e9afe0039b0f19ad1ebe1f8aba7e2
yoel3imari <youssef.elaimari@gmail.com>
2025-01-10 10:37:37 +0100

    check if .env exists

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
+import os
+
 def write_to_env(file_path: str, key: str, value: str):
     lines = []
     updated = False
+
+    # Check if the file exists, if not create it
+    if not os.path.exists(file_path):
+        open(file_path, "w").close()
 
     # Read the existing .env file
     with open(file_path, "r") as file:
